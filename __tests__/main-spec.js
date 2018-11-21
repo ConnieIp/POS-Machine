82d5dc6dc94a146eb5eea6119facc4c6e12b99d3
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 18:00:21 +0800

    testcase countBuyItems modify

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,9 +1,9 @@
 const printReciept = require("../main");
 
 describe('countBuyItems', function() {
-    it(`Given ['ITEM000001','ITEM000001'] and product list, When call countBuyItems, Then return a buy item list {barcode: 'ITEM000001', name:'Coca-Cola', unit:'bottles', price:3, quantity:2}`, function() {
+    it(`Given ['ITEM000001','ITEM000001'] and product list, When call countBuyItems, Then return a buy item list [{barcode: 'ITEM000001', name:'Coca-Cola', unit:'bottles', price:3, quantity:2}]`, function() {
         let idList = ['ITEM000001', 'ITEM000001'];
-        let result = printReciept.countBuyItems(idList, printReciept.loadAllItems);
-        expect(result).toEqual({ barcode: 'ITEM000001', name: 'Coca-Cola', unit: 'bottles', price: 3, quantity: 2 });
+        let result = printReciept.countBuyItems(idList, printReciept.loadAllItems());
+        expect(result).toEqual([{ barcode: 'ITEM000001', name: 'Sprike', unit: 'bottles', price: 3.00, quantity: 2 }]);
     });
 });
