aa425bbc524c1104a4eb800629404de703d0c8b9
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 22:56:15 +0800

    testcase caculateTotal

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -41,3 +41,11 @@ describe('calculatePromotionSubTotalPrice', function() {
         expect(result).toEqual([{ barcode: 'ITEM000001', name: 'Sprike', unit: 'bottles', price: 3.00, quantity: 3, subTotal: 9.00, promotionSubTotal: 6.00 }]);
     });
 });
+
+describe('calculateTotal', function() {
+    it(`Given [{ barcode: 'ITEM000001', name: 'Sprike', unit: 'bottles', price: 3.00, quantity: 3, subTotal: 9.00, promotionSubTotal: 6.00 },{ barcode: 'ITEM000002', name: 'Apple', unit: 'g', price: 5.50, quantity: 2, subTotal: 11.00}], When call calculateTotal, Then return 17.00`, function() {
+        let buyItemList = [{ barcode: 'ITEM000001', name: 'Sprike', unit: 'bottles', price: 3.00, quantity: 3, subTotal: 9.00, promotionSubTotal: 6.00 }, { barcode: 'ITEM000002', name: 'Apple', unit: 'g', price: 5.50, quantity: 2, subTotal: 11.00 }];
+        let result = printReciept.calculateTotal(buyItemList, printReciept.loadPromotions());
+        expect(result).toEqual(17.00);
+    });
+});
